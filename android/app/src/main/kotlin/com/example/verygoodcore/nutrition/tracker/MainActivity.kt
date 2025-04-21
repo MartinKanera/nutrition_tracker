13a790edfab75fd2e6ec4a6f01d5d560cb09472b
Martin Kaněra <marti.kanera@gmail.com>
2025-04-21 11:38:21 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/nutrition/tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.nutrition_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:nutrition_tracker/app/app.dart';
+import 'package:nutrition_tracker/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
