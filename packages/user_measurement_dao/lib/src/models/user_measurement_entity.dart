c327bead0ee30d7ba74bbb43b778cdd60af77cd2
Martin Kaněra <marti.kanera@gmail.com>
2025-04-26 16:32:32 +0200

    chore: satisfy linter

--- packages/user_measurement_dao/lib/src/models/user_measurement_entity.dart
+++ packages/user_measurement_dao/lib/src/models/user_measurement_entity.dart
@@ -4,8 +4,11 @@ part 'user_measurement_entity.g.dart';
 
 @Collection(accessor: 'userMeasurements')
 class UserMeasurementEntity {
-  UserMeasurementEntity(
-      {required this.id, required this.weight, required this.date});
+  UserMeasurementEntity({
+    required this.id,
+    required this.weight,
+    required this.date,
+  });
 
   final Id id;
   final double weight;
